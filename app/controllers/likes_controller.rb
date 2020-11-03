8595662c6580d6eade5fc79bb9e4504ec23707f5
Vivek Baruah <vivekbrh01@gmail.com>
2020-11-03 19:52:46 +0530

    Slug generated for URLs

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,7 +3,7 @@ class LikesController < ApplicationController
   before_action :require_signin
 
   def create
-    @event = Event.find(params[:event_id])
+    @event = Event.find_by!(slug: params[:event_id])    
     @event.likes.create!(user: current_user)
     redirect_to @event
   end
@@ -11,7 +11,7 @@ class LikesController < ApplicationController
   def destroy
     like = current_user.likes.find(params[:id])
     like.destroy
-    event = Event.find(params[:event_id])
+    event = Event.find_by!(slug: params[:event_id])
     redirect_to event
   end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,12 +1,15 @@
 class Event < ApplicationRecord
 
+  before_save :set_slug
+  
   has_many :registrations, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :likers, through: :likes, source: :user
   has_many :categorizations, dependent: :destroy
   has_many :categories, through: :categorizations
 
-  validates :name, :location, presence: true
+  validates :name, presence: true, uniqueness: true
+  validates :location, presence: true
 
   validates :description, length: { minimum: 25 }
 
@@ -32,5 +35,14 @@ class Event < ApplicationRecord
   end
   def sold_out?
     (capacity - registrations.size).zero?
+  end
+  def to_param
+    slug
+  end
+
+  private
+
+  def set_slug
+    self.slug = name.parameterize
   end
 end
