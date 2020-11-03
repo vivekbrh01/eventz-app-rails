67e51998fb364b894f6a1630fe8f3d9e6363718e
Vivek Baruah <vivekbrh01@gmail.com>
2020-11-03 08:45:29 +0530

    Registration using forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,6 @@
 Rails.application.routes.draw do
-
-  resources :registrations
   root "events#index"
-  # get "events" => "events#index"
-  # get "events/new" => "events#new"
-  # get "events/:id" => "events#show", as: "event"
-  # get "events/:id/edit" => "events#edit", as: "edit_event"
-  # patch "events/:id" => "events#update"
-
-  resources :events  
-
+  resources :events do 
+    resources :registrations
+  end
 end
